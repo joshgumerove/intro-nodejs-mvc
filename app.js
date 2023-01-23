2cab9c25a66db19b797cba5c1f1a6db9bbc6f4eb
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-23 17:18:52 -0500

    add model data

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const path = require("path");
-
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const pageNotFoundController = require("./controllers/error");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -17,8 +16,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(pageNotFoundController.getPageNotFound);
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const productsController = require("../controllers/products");
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get("/add-product", productsController.getAddProducts);
+router.get("/add-product", productsController.getAddProduct);
 
 // /admin/add-product => POST
 router.post("/add-product", productsController.postAddProduct);
