f95839cea524d93da9a1b74bd97e7ed181d544f3
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-31 04:20:59 -0500

    create new pages in applicattion

--- controllers/error.js
+++ controllers/error.js
@@ -1,5 +1,5 @@
 const getPageNotFound = (req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/" });
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/404" });
 };
 
 module.exports = { getPageNotFound };
